77e2f1d1990f9cf53a620e538bec698bb26ac449
Charlie Grozier <99926759+C-Groz@users.noreply.github.com>
2022-02-18 19:26:50 -0500

    uzi start

---
+++ guns/Uzi.js
@@ -0,0 +1,12 @@
+class Uzi extends SMG{
+    constructor(xPos, yPos){
+        super();
+        this.gLength = 25;
+        this.gWidth = 4;
+        this.xShift = 25;
+        this.yShift = -4;
+        this.bulletFireConstant = 50;
+
+        
+    }
+}
