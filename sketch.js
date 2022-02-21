e16b0fc85e0f50ac88bacd20d380e27211787a95
Charlie Grozier <99926759+C-Groz@users.noreply.github.com>
2022-02-21 00:32:10 -0500

    start menu

--- sketch.js
+++ sketch.js
@@ -25,185 +25,221 @@ function setup() {
   initializePickups();
   gameMap.drawMap();
   score.drawScoreLayout();
-  startRound();
+  gameActive = false;
+  //startRound();
+
+  startButton = createButton('Start Game');
 }
 
 
 function draw() {
   background(150);
-    
-  //update bullets
-  push();
-  for (var a = 0; a < bullets.length; a++){
-    fill(0, 0, 0);
-    circle(bullets[a].xPos, bullets[a].yPos, bullets[a].radius);
-    bullets[a].update();
 
-    for(var i = 0; i < enemies.length; i++){
-      if(enemies[i].contains(bullets[a].xPos, bullets[a].yPos)){
-        if(enemies[i].bulletInEnemy != a){
-          enemies[i].health -= currentGun.damage;
-          enemies[i].healthPercent = enemies[i].health/enemies[i].startingHealth * 100;
-        }
-        bullets[a].bulletInEnemy = i;
-        enemies[i].bulletInEnemy = a;
-      }else if(enemies.bulletInEnemy == a && bullets[a].bulletInEnemy == i){
-        enemies[i].bulletInEnemy = -1;
-        bullets[a].bulletInEnemy = -1;
-      }
-    }
-    
-    if(gameMap.anyRectangleContains(bullets[a].xPos, bullets[a].yPos)){
+  //start menu
+  if(!gameActive){
+    drawStartMenu();
+  }
+
+  if(gameActive){
+
+    //update bullets
+    push();
+    for (var a = 0; a < bullets.length; a++){
+      fill(0, 0, 0);
+      circle(bullets[a].xPos, bullets[a].yPos, bullets[a].radius);
+      bullets[a].update();
+
       for(var i = 0; i < enemies.length; i++){
-        if(enemies[i].bulletInEnemy == a && bullets[a].bulletInEnemy == i){
+        if(enemies[i].contains(bullets[a].xPos, bullets[a].yPos)){
+          if(enemies[i].bulletInEnemy != a){
+            enemies[i].health -= currentGun.damage;
+            enemies[i].healthPercent = enemies[i].health/enemies[i].startingHealth * 100;
+          }
+          bullets[a].bulletInEnemy = i;
+          enemies[i].bulletInEnemy = a;
+        }else if(enemies.bulletInEnemy == a && bullets[a].bulletInEnemy == i){
           enemies[i].bulletInEnemy = -1;
           bullets[a].bulletInEnemy = -1;
         }
       }
       
-      bullets[a].endRoute(a);
+      if(gameMap.anyRectangleContains(bullets[a].xPos, bullets[a].yPos)){
+        for(var i = 0; i < enemies.length; i++){
+          if(enemies[i].bulletInEnemy == a && bullets[a].bulletInEnemy == i){
+            enemies[i].bulletInEnemy = -1;
+            bullets[a].bulletInEnemy = -1;
+          }
+        }
+        
+        bullets[a].endRoute(a);
+      }
     }
-  }
-  pop();
+    pop();
 
-  //update enemies
-  for(var i = 0; i < enemies.length; i++){
-    enemies[i].drawEnemy();
-    enemies[i].determineTraj();
-    enemies[i].moveTowardPlayer();
-    if(enemies[i].playerContact()){
-      score.playerHealth -= enemies[i].damage;
+    //update enemies
+    for(var i = 0; i < enemies.length; i++){
+      enemies[i].drawEnemy();
+      enemies[i].determineTraj();
+      enemies[i].moveTowardPlayer();
+      if(enemies[i].playerContact()){
+        score.playerHealth -= enemies[i].damage;
+      }
+      if(enemies[i].health <= 0 && enemies[i].alive){
+        enemies[i].die(i);
+      }
     }
-    if(enemies[i].health <= 0 && enemies[i].alive){
-      enemies[i].die(i);
-    }
-  }
-  
-  checkSpawn();
-  
-  //update player
-  push();
-    translate(player1.x, player1.y);
-    player1.determineAngle();
-    rotate(player1.angle);
-    fill(0, 150, 0);
-    circle(0,0,50);
-    fill(30, 30, 30);
-    currentGun.drawGun(currentGun.xShift, currentGun.yShift);
-  pop();
+    
+    checkSpawn();
+    
+    //update player
+    push();
+      translate(player1.x, player1.y);
+      player1.determineAngle();
+      rotate(player1.angle);
+      fill(0, 150, 0);
+      circle(0,0,50);
+      fill(30, 30, 30);
+      currentGun.drawGun(currentGun.xShift, currentGun.yShift);
+    pop();
 
-  //update map
-  gameMap.move();
-  gameMap.drawMap();
-  score.drawScoreLayout();
+    //update map
+    gameMap.move();
+    gameMap.drawMap();
+    score.drawScoreLayout();
 
-  //pickups
-  pickups.forEach(element => {
-    if(element.playerInProximity()){
-      element.offerPickup();
-      if(keyIsDown(70) && !element.pickedUpBool){
-        element.userPickedUp();
-        element.pickedUpBool = true;
+    //pickups
+    pickups.forEach(element => {
+      if(element.playerInProximity()){
+        element.offerPickup();
+        if(keyIsDown(70) && !element.pickedUpBool){
+          element.userPickedUp();
+          element.pickedUpBool = true;
+        }
+        if(!keyIsDown(70)){
+          element.pickedUpBool = false;
+        }
+      }
+    });
+
+    //doors
+    doors.forEach(element => {
+      if(element.playerInProximity()){
+        element.offerInteraction();
+        if(keyIsDown(70) && !element.pickedUpBool){
+          element.userInteracted();
+          element.pickedUpBool = true;
+        }
+        if(!keyIsDown(70)){
+          element.pickedUpBool = false;
+        }
+      }
+    });
+
+    //mystery box
+    if(mbox.playerInProximity()){
+      if(!mbox.open){
+        mbox.offerInteraction();
+        mbox.pickedUpBool = false;
+      }
+
+      if(keyIsDown(70) && !mbox.pickedUpBool && !mbox.spinning && mbox.open){
+        mbox.userPickedUp();
+        mbox.pickedUpBool = true;
+        mbox.fPressed = true;
+      }
+      if(keyIsDown(70) && !mbox.open && score.money >= mbox.cost && !mbox.fPressed){
+        mbox.startSpin();
+        mbox.fPressed = true;
       }
       if(!keyIsDown(70)){
-        element.pickedUpBool = false;
+        mbox.fPressed = false;
       }
-    }
-  });
-
-  //doors
-  doors.forEach(element => {
-    if(element.playerInProximity()){
-      element.offerInteraction();
-      if(keyIsDown(70) && !element.pickedUpBool){
-        element.userInteracted();
-        element.pickedUpBool = true;
-      }
-      if(!keyIsDown(70)){
-        element.pickedUpBool = false;
-      }
-    }
-  });
-
-  //mystery box
-  if(mbox.playerInProximity()){
-    if(!mbox.open){
-      mbox.offerInteraction();
-      mbox.pickedUpBool = false;
-    }
-
-    if(keyIsDown(70) && !mbox.pickedUpBool && !mbox.spinning && mbox.open){
-      mbox.userPickedUp();
-      mbox.pickedUpBool = true;
-      mbox.fPressed = true;
-    }
-    if(keyIsDown(70) && !mbox.open && score.money >= mbox.cost && !mbox.fPressed){
-      mbox.startSpin();
-      mbox.fPressed = true;
-    }
-    if(!keyIsDown(70)){
-      mbox.fPressed = false;
-    }
-    
-    
-  }
-
-
-  //update round
-  if(enemyAmount == 0 && score.enemiesRemaining == 0){
-    score.round++;
-    startRound();
-    spawnTimer = spawnTimer - 20;
-  }
-
-  //when bullet fired
-  currentGun.shoot();
-
-  //reload
-  if((keyIsDown(82) && !score.reloading) || score.ammoIn == 0 && !score.reloading && ammoOut != 0){
-    currentGun.startReload();
-  }
-  if(score.reloading && currentGun.tempTimeEnd < millis()){
-    currentGun.reload();
-  }
-
-  }
-  function startRound(){
-    enemyHealth += 5;
-    
-    bullets = [];
-    enemies = [];
-    enemyAmount = score.round*5 + 5;
-    score.enemiesRemaining = enemyAmount;
-
-    for(var b = 0; b < (enemyAmount / 5); b++){
-      var temp = gameMap.randomSpawnPoint();
-      append(enemies, new Enemy(temp[0], temp[1]));
-    }
-    enemyAmount -= (enemyAmount / 5);
-    
-  }
-
-  function checkSpawn(){
-    if(millis() > nextChange && enemyAmount != 0){
-      var temp = gameMap.randomSpawnPoint();
-      append(enemies, new Enemy(temp[0], temp[1]));
-      nextChange = millis() + spawnTimer;
-      enemyAmount--;
-    }
-  }
- 
-  function initializePickups(){
-    pickups = [
-      ammoPickup = new AmmoPickup(-30, 400),
-      TopGunPickup = new GunPickup(150, -30, new M1911(player1.x, player1.y)),
-    ]
-    doors = [
-      door1 = new Door(1, 1000, 50, 25, [1]),
       
-    ]
-  }
+      
+    }
 
+
+    //update round
+    if(enemyAmount == 0 && score.enemiesRemaining == 0 && gameActive){
+      score.round++;
+      startRound();
+      spawnTimer = spawnTimer - 20;
+    }
+
+    //when bullet fired
+    currentGun.shoot();
+
+    //reload
+    if((keyIsDown(82) && !score.reloading) || score.ammoIn == 0 && !score.reloading){
+      currentGun.startReload();
+    }
+    if(score.reloading && currentGun.tempTimeEnd < millis()){
+      currentGun.reload();
+    }
+
+    }
+    
+}
+
+function startRound(){
+  enemyHealth += 5;
   
+  bullets = [];
+  enemies = [];
+  enemyAmount = score.round*5 + 5;
+  score.enemiesRemaining = enemyAmount;
 
+  for(var b = 0; b < (enemyAmount / 5); b++){
+    var temp = gameMap.randomSpawnPoint();
+    append(enemies, new Enemy(temp[0], temp[1]));
+  }
+  enemyAmount -= (enemyAmount / 5);
+  
+}
 
+function checkSpawn(){
+  if(millis() > nextChange && enemyAmount != 0){
+    var temp = gameMap.randomSpawnPoint();
+    append(enemies, new Enemy(temp[0], temp[1]));
+    nextChange = millis() + spawnTimer;
+    enemyAmount--;
+  }
+}
+ 
+function initializePickups(){
+  pickups = [
+    ammoPickup = new AmmoPickup(-30, 400),
+    TopGunPickup = new GunPickup(150, -30, new M1911(player1.x, player1.y)),
+  ]
+  doors = [
+    door1 = new Door(1, 1000, 50, 25, [1]),
+    door2 = new Door(2, 1500, 50, 25, [4, 5])
+  ]
+}
+
+function drawStartMenu(){
+  fill(150,44,0)
+  textSize(80);
+  text('Chank\'s Zombies', windowWidth/2 - 310, 150);
+  
+  let menuX = windowWidth/2 - 200;
+  let menuY = windowHeight/2 - 200;
+  let menuL = 400;
+  let menuH = 600;
+  //menu backround
+  fill(44, 44, 44);
+  rect(menuX, menuY, menuL, menuH, 50);
+
+  //buttons
+  startButton.position(menuX + 45, menuY + 50);
+  startButton.mousePressed(startGame);
+  startButton.size(menuL - 100, 100)
+
+}
+
+function startGame(){
+  gameActive = true;
+  startButton.hide();
+  startRound();
+  
+}
